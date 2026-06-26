9af6e493e5c01e77f9aa7bf4116484c7b6ecc35e
Arfa <arfakamal0212@gmail.com>
2026-06-27 01:56:40 +0530

    Day 3: Java Sorting Programs
    Added Bubble Sort, Selection Sort, Ascending Order, Descending Order, Sort Strings, Largest After Sorting, Smallest After Sorting, and Merge & Sort programs.

---
+++ MergeAndSort.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Array
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First Size: ");
+        int n1=sc.nextInt();
+        int arr1[]=new int[n1];
+        System.out.println("Enter First Array Elements: ");
+        for(int i=0;i<n1;i++)
+        {
+            arr1[i]=sc.nextInt();
+        }
+        System.out.println("Enter Second Size: ");
+        int n2=sc.nextInt();
+        int arr2[]=new int[n2];
+        System.out.println("Enter Second Array Elements: ");
+        for(int i=0;i<n2;i++)
+        {
+            arr2[i]=sc.nextInt();
+        }
+        int merge[]=new int[n1+n2];
+        for(int i=0;i<n1;i++)
+        {
+            merge[i]=arr1[i];
+        }
+        for(int i=0;i<n2;i++)
+        {
+            merge[n1+i]=arr2[i];
+        }
+        for(int i=0;i<n1+n2-1;i++)
+        {
+            for(int j=0;j<n1+n2-1-i;j++)
+            {
+                if(merge[j]>merge[j+1])
+                {
+                    int temp=merge[j];
+                    merge[j]=merge[j+1];
+                    merge[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Merged & Sort Array: ");
+        for(int i=0;i<n1+n2;i++)
+        {
+            System.out.println(merge[i]);
+        }
+    }
+}
